009ce0bae8f22b4dc8a9ba8640d9a89b9c850dae
djoudj-dev <nedellec.julien.78@gmail.com>
2025-07-29 15:02:49 +0200

    feat(config): améliore la configuration ESLint et met à jour les dépendances
    - Met à jour ESLint pour inclure le plugin vitest et configure les règles associées
    - Ajoute une configuration spécifique pour les fichiers `.spec.ts` avec les globals de Vitest
    - Adapte les règles Angular et TypeScript pour un meilleur respect des standards
    - Harmonise tous les imports en utilisant des guillemets doubles (`"`) pour le style de code
    - Met à jour le fichier `pnpm-lock.yaml` pour inclure les nouvelles dépendances et résolutions

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,10 @@
-import { Component, signal } from '@angular/core';
+import { Component, signal } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
+  selector: "app-root",
   imports: [],
-  templateUrl: './app.html',
-  styleUrl: './app.css'
+  templateUrl: "./app.html",
 })
 export class App {
-  protected readonly title = signal('ng-portfolio-app');
+  protected readonly title = signal("ng-portfolio-app");
 }
