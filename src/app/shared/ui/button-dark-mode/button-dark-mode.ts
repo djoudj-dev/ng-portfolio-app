d77f6358f4a686f1e5fea12d1a81531e3e2f498f
djoudj-dev <nedellec.julien.78@gmail.com>
2025-07-29 23:29:16 +0200

    feat(components): ajoute le bouton pour basculer en mode sombre
    - Crée le composant `ButtonDarkMode` avec gestion de thème via le `ThemeService`
    - Intègre un design interactif avec icônes et animations pour le mode sombre/clair
    - Ajoute des tests unitaires pour vérifier la création et le rendu du bouton

---
+++ src/app/shared/ui/button-dark-mode/button-dark-mode.ts
@@ -0,0 +1,33 @@
+import { Component, inject } from "@angular/core";
+import { ThemeService } from "@core/services/theme-service";
+
+@Component({
+  selector: "app-button-dark-mode",
+  imports: [],
+  template: `
+    <button
+      class="p-2 rounded-lg bg-background hover:bg-accent group"
+      (click)="toggleDarkMode()"
+      aria-label="Basculer le mode sombre"
+      title="Basculer le mode sombre"
+    >
+      <img
+        class="h-5 w-5 transition-all duration-300 ease-in-out group-hover:scale-110 group-hover:rotate-12 icon-invert"
+        [src]="isDarkMode() ? 'icons/moon.svg' : 'icons/sun.svg'"
+        [alt]="isDarkMode() ? 'Mode sombre activé' : 'Mode clair activé'"
+        width="20"
+        height="20"
+      />
+    </button>
+  `,
+})
+export class ButtonDarkMode {
+  private readonly themeService = inject(ThemeService);
+
+  isDarkMode = this.themeService.isDarkMode;
+  theme = this.themeService.theme;
+
+  toggleDarkMode() {
+    this.themeService.toggleTheme();
+  }
+}
