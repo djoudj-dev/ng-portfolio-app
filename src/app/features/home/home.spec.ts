157765ba5b9f19fea7e216e18138db0836cefce0
djoudj-dev <nedellec.julien.78@gmail.com>
2025-07-30 22:31:46 +0200

    test(shared): corrige et harmonise les tests unitaires des composants
    - Supprime les imports inutilisés dans `home.spec.ts` pour simplifier les dépendances
    - Met à jour la syntaxe des tests de `toast.spec.ts` avec des guillemets doubles pour uniformiser le style
    - Corrige le code de création du composant `Toast` pour éviter les erreurs dans les tests

--- src/app/features/home/home.spec.ts
+++ src/app/features/home/home.spec.ts
@@ -1,24 +1,22 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { TestingModule } from '@core/testing/testing.module';
+import { ComponentFixture, TestBed } from "@angular/core/testing";
 
-import { Home } from './home';
+import { Home } from "./home";
 
-describe('Home', () => {
+describe("Home", () => {
   let component: Home;
   let fixture: ComponentFixture<Home>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [Home, TestingModule]
-    })
-    .compileComponents();
+      imports: [Home, TestingModule],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(Home);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it("should create", () => {
     expect(component).toBeTruthy();
   });
 });
--- src/app/shared/ui/toast/toast.spec.ts
+++ src/app/shared/ui/toast/toast.spec.ts
@@ -1,23 +1,21 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from "@angular/core/testing";
+import { Toast } from "@shared/ui/toast/model/toast-model";
 
-import { Toast } from './toast';
-
-describe('Toast', () => {
+describe("Toast", () => {
   let component: Toast;
   let fixture: ComponentFixture<Toast>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [Toast]
-    })
-    .compileComponents();
+      imports: [Toast],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(Toast);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it("should create", () => {
     expect(component).toBeTruthy();
   });
 });
