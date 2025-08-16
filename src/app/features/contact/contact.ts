8be8a99c9b8367561e28a707c5fa0a53b822ba65
djoudj-dev <nedellec.julien.78@gmail.com>
2025-08-16 08:51:56 +0200

    feat(contact): ajoute un bouton de retour en haut et améliore la section contact
    - Implémente un bouton fixe pour naviguer vers l'accueil avec une icône dédiée
    - Corrige les chemins des assets SVG dans les composants `ContactStatusMessage`
    - Réorganise les structures HTML et améliore la sémantique avec l'utilisation des balises `header`
    - Traduit les attributs `alt` pour les icônes en français
    - Injecte le service `Router` et ajoute la méthode `navigateToTop()` dans le composant `Contact`

--- src/app/features/contact/contact.ts
+++ src/app/features/contact/contact.ts
@@ -1,6 +1,7 @@
-import { Component, signal } from "@angular/core";
+import { Component, inject, signal } from "@angular/core";
 import { NgOptimizedImage } from "@angular/common";
 import { ReactiveFormsModule } from "@angular/forms";
+import { Router } from "@angular/router";
 import { ContactCardGroup } from "./interface/contact.interface";
 import { CONTACT_DATA } from "./data/contact-data";
 import { ContactMessageCard } from "@features/contact/components/contact-message-card/contact-message-card";
@@ -11,6 +12,8 @@ import { ContactMessageCard } from "@features/contact/components/contact-message
   templateUrl: "./contact.html",
 })
 export class Contact {
+  private readonly router = inject(Router);
+  
   readonly contactTitle = signal<string>("Contactez-moi");
   readonly contactSubTitle = signal<string>(
     "N'hésitez pas à me contacter pour toute question ou proposition de collaboration.",
@@ -19,4 +22,8 @@ export class Contact {
   readonly contactCardGroups = signal<ContactCardGroup[]>(
     CONTACT_DATA.cardGroups,
   );
+
+  navigateToTop(): void {
+    this.router.navigate(['/']);
+  }
 }
