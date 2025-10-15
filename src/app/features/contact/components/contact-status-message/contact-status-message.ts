6b9183a6f9b9cc21f94c9e2dab9899103f428e8d
djoudj <contact@nedellec-julien.fr>
2025-10-15 21:31:10 +0200

    feat: Ajouter une méthode pour extraire le SVG d'un sprite et mise à jour des icônes dans le composant de contact

--- src/app/features/contact/components/contact-status-message/contact-status-message.ts
+++ src/app/features/contact/components/contact-status-message/contact-status-message.ts
@@ -1,36 +1,31 @@
-import { NgOptimizedImage } from "@angular/common";
-import { Component, input, output, ChangeDetectionStrategy } from "@angular/core";
-import { ButtonComponent } from "@shared/ui/button/button";
+import { Component, input, output, ChangeDetectionStrategy } from '@angular/core';
+import { ButtonComponent } from '@shared/ui/button/button';
+import { SvgIcon } from '@app/shared/ui/icon-svg/icon-svg';
 
-export type SubmissionStatus = "success" | "error";
+export type SubmissionStatus = 'success' | 'error';
 
 @Component({
-  selector: "app-contact-status-message",
-  imports: [ButtonComponent, NgOptimizedImage],
+  selector: 'app-contact-status-message',
+  imports: [ButtonComponent, SvgIcon],
   changeDetection: ChangeDetectionStrategy.OnPush,
   template: `
     <div class="flex flex-col items-center justify-center py-14">
       <div class="text-center">
-        @if (status() === "success") {
-          <img
-            [ngSrc]="'/icons/success.svg'"
-            alt="Error icon"
+        @if (status() === 'success') {
+          <app-svg-icon
+            [name]="'lucide:check-circle'"
             class="mx-auto h-48 w-48"
             width="128"
             height="128"
           />
 
-          <p class="text-text mb-2 text-xl font-bold">
-            Message envoyé avec succès!
-          </p>
+          <p class="text-text mb-2 text-xl font-bold">Message envoyé avec succès!</p>
           <p class="text-text/70 mb-6 text-center">
-            Merci pour votre message. Je vous répondrai dans les plus brefs
-            délais.
+            Merci pour votre message. Je vous répondrai dans les plus brefs délais.
           </p>
         } @else {
-          <img
-            [ngSrc]="'/icons/error.svg'"
-            alt="Error icon"
+          <app-svg-icon
+            [name]="'lucide:x-circle'"
             class="mx-auto h-48 w-48"
             width="128"
             height="128"
@@ -38,18 +33,13 @@ export type SubmissionStatus = "success" | "error";
 
           <p class="text-text mb-2 text-xl font-bold">Erreur d'envoi</p>
           <p class="text-text/70 mb-6 text-center">
-            Une erreur s'est produite lors de l'envoi de votre message. Veuillez
-            réessayer.
+            Une erreur s'est produite lors de l'envoi de votre message. Veuillez réessayer.
           </p>
         }
       </div>
 
-      <app-button
-        color="accent"
-        customClass="px-6 py-2"
-        (buttonClick)="tryAgain.emit()"
-      >
-        {{ status() === "success" ? "Envoyer un autre message" : "Réessayer" }}
+      <app-button color="accent" customClass="px-6 py-2" (buttonClick)="tryAgain.emit()">
+        {{ status() === 'success' ? 'Envoyer un autre message' : 'Réessayer' }}
       </app-button>
     </div>
   `,
