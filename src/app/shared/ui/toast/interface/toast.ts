c68fea73935e591e8d6bcaa2ffa8a6914b26d6a9
djoudj <contact@nedellec-julien.fr>
2025-10-18 18:12:45 +0200

    🔥 refactor(toast): simplifie et allège la gestion des toasts
    - Supprime la configuration `ToastConfig` et les directives associées
    - Réduit le code redondant dans `ToastService` et simplifie sa logique
    - Assure une structure plus directe pour les composants de toast
    - Ajuste les animations et styles pour un rendu plus fluide et moderne

--- src/app/shared/ui/toast/interface/toast.ts
+++ src/app/shared/ui/toast/interface/toast.ts
@@ -1,22 +1,8 @@
-export type ToastType = 'success' | 'warning' | 'danger';
+export type ToastType = 'success' | 'danger';
 
 export interface ToastData {
   id: string;
   type: ToastType;
   title: string;
   message?: string;
-  duration?: number;
-  dismissible?: boolean;
-}
-
-export interface ToastConfig {
-  position?:
-    | 'top-right'
-    | 'top-left'
-    | 'bottom-right'
-    | 'bottom-left'
-    | 'top-center'
-    | 'bottom-center';
-  defaultDuration?: number;
-  maxToasts?: number;
 }
